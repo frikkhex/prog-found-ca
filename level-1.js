b2abbe3e6ee90c521a1f7e0fcf561c23eabb965c
frikkhex <67097261+frikkhex@users.noreply.github.com>
2020-10-25 20:27:02 +0100

    CA Answers, level-1

--- level-1.js
+++ level-1.js
@@ -1,13 +1,13 @@
-// 1. Declare and initialise a variable with a string value.
+// 1. 
 var name = "Frikk";
 
-// 2. Create an object variable called person and give it two properties (a key and a value) of your choice.
+// 2. 
 var person = {
           name: "Frikk",
           age: 28
 };
 
-// 3. Create a variable called outOfStock and assign it a boolean value. Create an if else statement that checks the value of outOfStock. If it is true, console log "Out of stock". Otherwise log "In stock".
+// 3. 
 var outOfStock = true;
 
 if(outOfStock === true){
@@ -17,30 +17,25 @@ else {
           console.log("The item is in stock");
 }
 
-// 4. Create an array of five numbers. Loop through the array and console log each value.
+// 4. 
 var numbers = [1, 2, 3, 4, 5];
 for (var i = 0; i < numbers.length; i++){
           console.log(numbers[i]);
 }
 
-// 5. Create a for loop that counts from 15 to 25. Console log the value of the counter variable inside the loop
+// 5. 
 for(var i = 15; i <= 25; i++) {
           console.log(i);
 }
 
-// 6. Using the same loop from the above question, only log the value of the counter variable if it is equal to 20.
+// 6. 
 for(var i = 15; i <= 25; i++) {
           if(i === 20){
           console.log(i);
 }
 }
 
-// 7. Create an array of two objects. Each object must have the same three properties (with different values):
-// one property with a string value
-// one property with a number value
-// one property with a boolean value
-// Loop through the array and console log the number value and the boolean value.
-
+// 7. 
 var familyMembers = [
           {
                     name: "Kurt",
@@ -59,17 +54,15 @@ for(var i = 0; i < familyMembers.length; i++){
           console.log(familyMembers[i].alive);
 }
 
-// 8. Create a function called whatIDontLike that accepts one argument. Choose a good name for the argument.
+// 8. 
 function whatIDontLike (notLikeable){
 
-// Inside the function, log the string "I don't like " together with the argument.          
 console.log("I don't like " + notLikeable);
 }
 
-// Inside the function, subtract the second argument from the first and console log the result.
 whatIDontLike("spiders");
 
-// 9. Create a function that accepts two arguments. Inside the function, subtract the second argument from the first and console log the result.
+// 9. 
 function ageDifference(age1, age2){
           var sum = age1 - age2;
           console.log("The age difference is " + sum);
@@ -78,10 +71,8 @@ function ageDifference(age1, age2){
 ageDifference(58, 27);
 
 // 10. 
-//Create an empty array.
 var favoriteSport = [];
 
-// Create a function that accepts one argument.
 function sport (type){
           favoriteSport.push(type);
           console.log("My favorite sport is " + type);
